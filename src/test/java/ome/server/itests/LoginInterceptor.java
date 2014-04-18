7106708a53b6a55b1289785c8dfbca7e773b7096
ssimard <ssimard@pasteur.fr>
2014-04-18 18:48:47 +0200

    Add ome.testing and ome.server.itest sources

---
+++ src/test/java/ome/server/itests/LoginInterceptor.java
@@ -0,0 +1,63 @@
+/*
+ *   Integration testing class not distributed with the OME artifacts.
+ *
+ *   Copied from:
+ *   https://github.com/openmicroscopy/openmicroscopy/blob/v.5.0.0/components/server/test/ome/server/itests/LoginInterceptor.java
+ *
+ *   See also:
+ *   https://github.com/openmicroscopy/openmicroscopy/blob/v.5.0.0/components/server/test/ome/server/itests/AbstractManagedContextTest.java#L145
+ *
+ *
+ *
+ *   $Id$
+ *
+ *   Copyright 2008 Glencoe Software, Inc. All rights reserved.
+ *   Use is subject to license terms supplied in LICENSE.txt
+ */
+package ome.server.itests;
+
+import java.util.Map;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import ome.security.basic.CurrentDetails;
+import ome.system.Principal;
+
+/**
+ * Simple interceptor used to control login on all method calls.
+ */
+public class LoginInterceptor implements MethodInterceptor {
+
+    final CurrentDetails cd;
+    public Principal p;
+    public Map<String, String> callContext = null;
+
+    public LoginInterceptor(CurrentDetails cd) {
+        this.cd = cd;
+    }
+
+    public Object invoke(MethodInvocation arg0) throws Throwable {
+        int still;
+        still = cd.size();
+        if (still != 0) {
+            throw new RuntimeException(still + " remaining on login!");
+        }
+
+        if (p != null) {
+            cd.login(p);
+            cd.setContext(callContext);
+        }
+
+        try {
+            return arg0.proceed();
+        } finally {
+            cd.setContext(null);
+            still = cd.logout();
+            if (still != 0) {
+                throw new RuntimeException(still + " remaining on logout!");
+            }
+        }
+    }
+
+}

201efabbe6a9a860b7fa9783626b15a2700d5705
ssimard <ssimard@pasteur.fr>
2015-11-20 15:36:52 +0100

    Drop 5.1.x database resources

--- src/test/resources/db/fixtures/omero51x/V9_9_9_1__dbunit_base_groups.sql
+++
@@ -1,3 +0,0 @@
--- common pre-existing local groups
-insert into experimentergroup (id,permissions,version,name,ldap)
-        values (ome_nextval('seq_experimentergroup'),-120,0,'OmeroUnitLocal',false);
